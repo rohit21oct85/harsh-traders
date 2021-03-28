915c4104a04efa4c39417916ad2ba9ba03daa20a
Rohit Singh <rohit21oct85@gmail.com>
2021-03-28 20:32:07 +0530

    code changed for login design

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React , {useState, useEffect, useRef} from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/login.module.css';
+import Image from 'next/image'
 import {Form, Button } from 'react-bootstrap';
 
-
+const myLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 50}`
+}
 export default function Login() {
   const emailRef = useRef();  
   const passwordRef  = useRef(); 
@@ -13,40 +14,47 @@ export default function Login() {
 
   }
   return (
-    <div className="container">
+    <div style={{ background: '#051438',width: '100%', height: '100vh'}}>
       <Head>
         <title>Admin Login :: HT </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="col-md-12 ">
-        <h1 className={styles.title}></h1>
-        <div className="container">
-            <div className="text-center card col-md-4 offset-4 pt-5 pb-5" style={{ margin: '12% auto 0%' }}>
-                <h3>HT Admin</h3>
-                <hr />
-            <Form autoComplete="new-password" onSubmit={submitForm}>
-                    <Form.Group controlId="formBasicEmail" className="text-left">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" autoComplete="nope" ref={emailRef} placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword"  className="text-left">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" autoComplete="nope" ref={passwordRef} placeholder="Password" />
-                    </Form.Group>
-                    <Button 
-                        className="btn btn-md btn-block dark mt-3" 
-                        type="submit"
-                    >
-                        {loading ? 'Authenticating...':'Login'}
-                    </Button>
-                    </Form>
+      <main className="row col-md-12">
+            <div style={{ padding: '0', position: 'fixed', left: '5%', top: '5%', bottom: '25%'}}>
+                <Image 
+                  loader={myLoader}
+                  src="/image/bg.jpg"
+                  alt="Picture of the author"
+                  width={750}
+                  height={650}
+               />
             </div>
-        </div>
+            
+            <div style={{ width:'400px',height: '380px', background:'#fff', padding: '26px', position: 'fixed', right: '10%',bottom:'25%', top: '25%'}}>
+                <h3>Harsh Traders Admin</h3>
+                <hr />
+                <Form autoComplete="new-password" onSubmit={submitForm}>
+                <Form.Group controlId="formBasicEmail" className="text-left">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" autoComplete="nope" ref={emailRef} placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword"  className="text-left">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" autoComplete="nope" ref={passwordRef} placeholder="Password" />
+                </Form.Group>
+                <Button 
+                    className="btn btn-md btn-block dark mt-3" 
+                    type="submit"
+                >
+                    {loading ? 'Authenticating...':'Login'}
+                </Button>
+                </Form>
+            </div>
       </main>
     </div>
   )
